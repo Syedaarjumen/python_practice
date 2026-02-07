4db1fcac94871f032dda9f47088bdc704bfa0e18
Your Name <youremail@example.com>
2026-02-07 20:02:50 +0530

    Added prime number program

---
+++ prime.py
@@ -0,0 +1,13 @@
+num = 11
+is_prime = True
+
+if num <= 1:
+    is_prime = False
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            is_prime = False
+            break
+
+print("Prime" if is_prime else "Not Prime")
+
